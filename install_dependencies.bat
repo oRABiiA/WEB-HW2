a378c3c6506a3ee8a8f30fbe8776a4323a96b490
oRABiiA <Rabealahham2@gmail.com>
2024-07-23 22:40:33 +0300

    Included .bat file

---
+++ install_dependencies.bat
@@ -0,0 +1,12 @@
+@echo off
+echo Installing dependencies...
+npm install
+npm install tw-elements-react
+npm install react-icons
+npm install aos
+npm install react aos
+npm install gsap @gsap/react
+npm install prop-types
+npm install react-tailwindcss-select
+echo All dependencies installed.
+
